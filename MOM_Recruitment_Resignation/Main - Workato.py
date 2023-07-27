b55e5dbac6a0cede828ce64560b20774c18c07d6
benedict733 <wbenedict733@gmail.com>
2023-07-27 15:28:01 +0800

    all files and documentations for MOM Recruitment and Resignation Rate extraction

---
+++ MOM_Recruitment_Resignation/Main - Workato.py
@@ -0,0 +1,47 @@
+# Motivation: 
+# Recruitment rate: The recruitment rate is useful as another data source to compare for 
+# output of tech talent. 
+# Resignation rate: The resignation rate is useful to compare and see if there is a higher
+# than normal resignation rate for the Infocomm sector. 
+
+import requests
+import pandas as pd
+import datetime as dt
+
+final_filename = "MOM_RnR.xlsx"
+
+rec_file = requests.get("https://stats.mom.gov.sg/iMAS_Tables1/Time-Series-Table/mrsd_25_Qtly_and_annl_tsd_on_rec_by_ind_and_occ_grp.xlsx", allow_redirects=True)
+resg_file = requests.get("https://stats.mom.gov.sg/iMAS_Tables1/Time-Series-Table/mrsd_26_Qtly_and_annl_tsd_on_resgn_by_ind_and_occ_grp.xlsx", allow_redirects=True)
+
+#create dct as dictionary for mapping of column names
+start_year = 2006; end_year = dt.datetime.now().year
+year_range = end_year - start_year + 1
+
+dct = {}
+
+for i in range (year_range): #year index
+    for j in range(1,5): #num quarters
+        str = f"{j}Q" if i == 0 else f"{j}Q.{i}"
+        dct[str] = f"{start_year} {j}Q"    
+    start_year += 1
+
+
+index_infocomm = 22; lst_index = [0, *range(index_infocomm, index_infocomm+3)]
+
+df_rec = pd.read_excel(rec_file.content, sheet_name=3, header=3).drop(columns="OCCUPATIONAL GROUP").rename(columns=dct)
+df_resg = pd.read_excel(resg_file.content, sheet_name=3, header=3).drop(columns="OCCUPATIONAL GROUP").rename(columns=dct)
+
+rec = df_rec.iloc[lst_index]
+resg = df_resg.iloc[lst_index]
+
+# write the dataframes into an excel file with 2 different sheet names
+with pd.ExcelWriter(final_filename) as writer:
+    rec.to_excel(writer, sheet_name="MOM Recruitment Rate", index=False)
+    resg.to_excel(writer, sheet_name="MOM Resignation Rate", index=False)
+
+# else can also write into 2 separate csv files
+# rec.to_csv("MOM_Recruitment_Rate.csv", index=False)
+# resg.to_csv("MOM_Resignation_Rate.csv", index=False)
+
+# os.remove(rec_filename)
+# os.remove(resg_filename)
